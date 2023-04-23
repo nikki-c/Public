eaf9c61c830f8dc8959cf10f684db5bb1877f2cf
Nikki Chapple <69439002+nikki-c@users.noreply.github.com>
2023-04-23 23:06:44 +0100

    Bulk add container sensitivity label to sites
    Uses PnP PowerShell.
    Bulk add container sensitivity label to sites using an input CVS file. If the site is connected to a group or team then the label is added to the group or team.

---
+++ Bulk_Add_Container_Sensitivity_Labels.ps1
@@ -0,0 +1,46 @@
+######################################################################################################
+# Bulk update container sensitivity label to Sharepoint sites based on CSV input
+# If the site has a Microsoft 365 Group behind it, the label will be placed on the Microsoft 365 Group
+# CSV format: 2 columns URL, LabelID. Note must use the sensitivyt label guid
+# Use PnP PowerShell. Require# V7 PowerShell
+# Last updated 23 April 2023
+######################################################################################################
+$orgName="<YOUR TENANT NAME>" 
+$tenantURL = "https://$orgName.sharepoint.com"
+
+$csvfile = "<CSV FILE PATH>"
+$ExportLocation = "<CSV FILE PATH>"
+
+#Install-Module PnP.PowerShell
+#Import-Module PnP.PowerShell
+# Register-PnPManagementShellAccess
+Connect-PnPOnline -Url $tenantURL -Interactive
+
+#Import CSV file
+$sites = Import-CSV -path $csvfile  
+
+# List
+$List = @()
+
+#For each site in the CSV
+ForEach ($site in $sites) {
+ 
+    #Get Parameters from CSV
+    $SiteURL = $site.URL
+    $LabelId = $site.LabelID  
+
+    #Add sensitivity Label to site
+    Set-PnPTenantSite -Identity $SiteURL -SensitivityLabel $LabelId 
+
+    $label = Get-PnPSiteSensitivityLabel
+
+    $Object = [PSCustomObject]@{
+        URL = $site.URL
+        Sensitivitylabel= $label.DisplayName
+        }
+    $List += $Object
+
+    }
+
+#Create the output report
+$List | Export-CSV $exportLocation -NoTypeInformation
